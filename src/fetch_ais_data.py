f0ee3ff64ae897b8801b548a629fdb79464f70f6
s0914712 <143980460+s0914712@users.noreply.github.com>
2026-02-08 10:01:21 +0800

    Update fetch_ais_data.py

--- src/fetch_ais_data.py
+++ src/fetch_ais_data.py
@@ -18,7 +18,7 @@ from collections import defaultdict
 
 # 配置
 API_KEY = os.environ.get('AISSTREAM_API_KEY', '').strip()
-TAIWAN_BBOX = [[[20, 112], [28, 130]]] 
+TAIWAN_BBOX = [[112.0, 20.0], [130.0, 28.0]]
 COLLECTION_TIME = 180  # 收集 3 分鐘的資料
 OUTPUT_FILE = 'data/ais_snapshot.json'
 
